8ffe2a510f69a6765facf55e09126f35db5869c7
Gavin Boulton <gavboulton@gmail.com>
2021-01-29 10:44:23 +0000

    [WIP] Create an alternative install list

---
+++ install.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env sh
+
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install Xcode command line tools
+xcode-select --install
+
+# Works repeatedly
+echo "Installing Brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# echo "Install aws/tap"
+# brew tap aws/tap
+
+echo "Install brews..."
+# brew install awscli \
+#  aws-sam-cli \
+#  go \
+#  ipython \
+#  jq \
+#  jupyterlab \
+#  kops \
+#  kubectx \
+#  kubernetes-cli \
+#  kustomize \
+#  skaffold \
+#  telnet \
+brew install jq \
+  yarn
+
+echo "Install casks..."
+# brew install --cask alfred \
+#  brave-browser \
+#  divvy \
+#  docker \
+#  iterm2 \
+#  firefox \
+#  google-chrome \
+#  microsoft-excel \
+#  microsoft-word \
+#  postico \
+#  postman \
+#  slack \
+#  spotify \
+#  visual-studio-code \
+#  vlc \
+#  whatsapp \
+#  webstorm \
+#  zoomus
+
+brew install --cask brave-browser \
+  divvy \
+  docker \
+  firefox \
+  iterm2 \
+  phpstorm \
+  postico \
+  postman \
+  sequel-pro \
+  spotify \
+  visual-studio-code \
+  webstorm \
+  zoomus
+
+
+echo "Installing NVM..."
+# Sets current profile i.e. .bashrc, .zshrc etc.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+# Conditionally create dev directory in user root
+[ ! -d ~/dev ] && mkdir ~/dev
