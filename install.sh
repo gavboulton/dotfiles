6a57a892b346220600e9d1f349b8e623d73f5eb8
hl <hl@Gavins-MacBook-Pro-Intel.local>
2022-01-11 16:16:31 +0000

    Separate shell configuration
    Seperate shell configuration from software installs so it can be run in separate user accounts.

--- install.sh
+++ install.sh
@@ -49,10 +49,3 @@ brew install --cask alfred \
   whatsapp \
   webstorm \
   zoomus
-
-echo "Installing NVM..."
-# Sets current profile i.e. .bashrc, .zshrc etc.
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-
-# Conditionally create dev directory in user root
-[ ! -d ~/dev ] && mkdir ~/dev
